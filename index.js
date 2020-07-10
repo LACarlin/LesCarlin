ea5c28f119e8a3adff6083cb94a42ef1b3b2d98e
LACarlin <linzicarlin@gmail.com>
2020-07-10 12:26:15 +0100

    set up for nodemailer

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 // const ejs = require("ejs");
+const nodemailer = require("nodemailer");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 // app.set("view engine", "ejs");
 
@@ -17,6 +22,9 @@ app.get("/contact", function(req, res){
     res.sendFile(__dirname + "/views/contact.html")
 });
 
+app.post("/contact", function(req, res){
+
+});
 
 app.listen(3000, function(){
     console.log("server started on port 3000")
